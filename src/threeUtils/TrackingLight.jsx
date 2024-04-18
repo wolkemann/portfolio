60fe049a3179ff7541ad6d84adc7c4e7f73a92d1
wolkemann <ohtoristudent@gmail.com>
2024-04-18 19:04:57 +0200

    feature/planeGeometry - implemented shadows on background

--- src/threeUtils/TrackingLight.jsx
+++ src/threeUtils/TrackingLight.jsx
@@ -4,15 +4,20 @@ import { useRef } from "react";
 export default function TrackingLight(props) {
   const light = useRef();
   useFrame((state) => {
-    if (
-      light.current.position.x > -0.4 &&
-      state.pointer.x > -0.4 &&
-      light.current.position.x < 0.5 &&
-      state.pointer.x < 0.5
-    ) {
-      light.current.position.x = state.pointer.x;
-    }
+    light.current.position.x = state.pointer.x * 1.5;
+    light.current.position.y = state.pointer.y;
   });
 
-  return <directionalLight ref={light} {...props} />;
+  return (
+    <spotLight
+      ref={light}
+      intensity={20}
+      position={[0, 0, 6]}
+      angle={0.05}
+      shadow-mapSize-width={2048}
+      shadow-mapSize-height={2048}
+      castShadow
+      {...props}
+    />
+  );
 }
