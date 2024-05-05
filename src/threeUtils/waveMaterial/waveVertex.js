4f5bf89cb9ac692641cd6aacea53ffde62f4e54f
wolkemann <ohtoristudent@gmail.com>
2024-05-05 11:47:51 +0200

    removed files

--- src/threeUtils/waveMaterial/waveVertex.js
+++
@@ -1,9 +0,0 @@
-export const waveVertex = `void main() {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.y += sin(modelPosition.x * 4.0) * 0.2;
-
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectedPosition = projectionMatrix * viewPosition;
-
-  gl_Position = projectedPosition;
-}`;
