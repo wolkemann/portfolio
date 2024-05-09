753925b91f2a2626c4f1ccad4093392bb5a2b69e
wolkemann <ohtoristudent@gmail.com>
2024-05-10 00:26:16 +0200

    ambientlight

--- src/scenes/AboutScene.jsx
+++ src/scenes/AboutScene.jsx
@@ -9,11 +9,14 @@ export default function AboutScene() {
       <Background color={0x0a0824} position={[0, 0, -5]} />
 
       <group>
-        <Woman shaderColor={{red: 0, blue: 0.1, green: 0.0}} position={[0, -0.95, 0.0]} />
-        <Lentes glassColor={'pink'} scale={0.0225} />
+        <Woman
+          shaderColor={{ red: 0, blue: 0.1, green: 0.0 }}
+          position={[0, -0.95, 0.0]}
+        />
+        <Lentes glassColor={"black"} scale={0.0225} />
       </group>
 
-      <spotLight angle={0.1} distance={1} penumbra={0.5} intensity={0.5} />
+      <ambientLight intensity={5} />
 
       <EffectComposer>
         <Noise opacity={0.05} />
