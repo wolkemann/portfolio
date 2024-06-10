926cbc902a0a6d8b63723791bbb49484bb2b3aec
Federico Sardo <federico.sardo@vistaglobal.com>
2024-06-10 16:05:32 +0200

    last

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,3 +1,5 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
 import Button from "../Button/Button";
 
 export default function ProjectCard({
@@ -6,28 +8,41 @@ export default function ProjectCard({
   tech,
   gitUrl,
   demoUrl,
+  website,
 }) {
   return (
     <div className="w-full h-full snap-always snap-center md:py-10 md:flex">
-      <div className="max-w-full bg-gray-50 rounded shadow-lg md:m-auto md:w-[900px]">
-        <div className="p-5 md:p-10">
-          <h4 className="mb-5 text-2xl font-black tracking-widest md:mb-10 md:text-2xl xl:text-5xl">
-            {name}
-          </h4>
+      <div className="max-w-full p-5 md:p-10 bg-gray-50 rounded shadow-lg md:m-auto md:w-[900px]">
+        <h4 className="flex items-center gap-2 mb-5 text-xl font-black tracking-widest md:mb-10 md:text-2xl xl:text-5xl">
+          {name}
+          {website && <a href={website} target="_BLANK">
+            <Icon
+              icon="eva:external-link-outline"
+              className="text-projects-title text-2xl md:text-4xl"
+            />
+          </a>}
+        </h4>
 
-          <p className="mb-5 tracking-widest leading-relaxed md:text-l xl:text-xl">
-            {description}
+        <p className="mb-5 tracking-widest leading-relaxed text-xs md:text-l xl:text-xl">
+          {description}
+        </p>
+        {tech && (
+          <p className="text-right mb-5 tracking-widest leading-relaxed text-sm md:text-l xl:text-xl">
+            <span className="font-black">Made with - </span>
+            {tech.join(", ")}
           </p>
-          {tech && (
-            <p className=" text-right mb-5 tracking-widest leading-relaxed md:text-l xl:text-xl">
-              <span className="font-black">Made with - </span>
-              {tech.join(",")}
-            </p>
+        )}
+        <div className="flex flex-col text-sm md:text-l xl:text-xl md:flex-row gap-3 md:gap-10">
+          {gitUrl && (
+            <Button href={gitUrl} icon="mdi:github">
+              GitHub
+            </Button>
           )}
-          <div className="flex gap-5 md:gap-10">
-            {gitUrl && <Button href={gitUrl}>GitHub</Button>}
-            {demoUrl && <Button href={demoUrl}>Try demo</Button>}
-          </div>
+          {demoUrl && (
+            <Button href={demoUrl} icon="material-symbols:play-circle">
+              Try it
+            </Button>
+          )}
         </div>
       </div>
     </div>
--- src/pages/ProjectsSection.jsx
+++ src/pages/ProjectsSection.jsx
@@ -1,4 +1,4 @@
-import { useInView } from "framer-motion";
+import { useInView, useSpring, motion, useScroll } from "framer-motion";
 import { useEffect, useRef } from "react";
 import { usePoseChanging } from "../hooks/usePoseChanging";
 import { SECTIONS, WOMAN_POSES } from "../utils/constants";
@@ -8,6 +8,13 @@ export default function ProjectsSection() {
   const { updateScene } = usePoseChanging();
   const ref = useRef(null);
   const isInView = useInView(ref);
+  const projectsRef = useRef(null);
+  const { scrollY } = useScroll({container: projectsRef});
+  const scaleX = useSpring(scrollY, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
 
   useEffect(() => {
     if (isInView) {
@@ -17,14 +24,18 @@ export default function ProjectsSection() {
 
   return (
     <section className="container snap-center" ref={ref} id={SECTIONS.PROJECTS}>
-      <div className="w-screen h-screen p-5 flex flex-col-reverse md:flex-row md:items-center md:gap-10 md:p-10">
-        <div className="w-full h-full overflow-y-scroll snap-mandatory snap-y md:max-w-[700px] xl:max-w-[900px]">
+      <motion.div className="fixed bottom-0 h-[10px] bg-projects-title" style={{width: scrollY}}></motion.div>
+      <div className="w-screen h-screen p-5 flex flex-col-reverse relative md:flex-row md:items-center md:gap-10 md:p-10">
+        <div
+          className="w-full h-full overflow-y-scroll snap-mandatory snap-y md:max-w-[700px] xl:max-w-[100%]"
+          ref={projectsRef}
+        >
           {projects.map((project) => (
             <ProjectCard key={project.name} {...project} />
           ))}
         </div>
 
-        <h3 className="text-projects-title font-black mb-5 md:mb-0 text-2xl self-end md:flex md:flex-col md:tracking-widest md:text-5xl xl:text-7xl">
+        <h3 className="text-projects-title font-black mb-5 md:mb-0 text-2xl self-start md:self-end md:flex md:flex-col md:tracking-widest md:text-5xl xl:text-7xl">
           <span>MY</span> <span>PROJECTS</span>
         </h3>
       </div>
@@ -37,14 +48,17 @@ const projects = [
     name: "Gentle Letters",
     description:
       "Gentle Letters is an anonymous and relaxing social App where you can send letters and wait a response from a random stranger who is willig to help you. As a token of appreciation for your helper's precious words, you can can send a sticker to let them know you read the response and appreciated it.",
-    tech: ["Next.js", ""],
+    tech: ["Next.js", "styled components"],
     gitUrl: "https://github.com/wolkemann/capstone-project",
-    demoUrl: "https://gentle-letters.vercel.app",
+    demoUrl: "",
+    website: "https://gentle-letters.vercel.app",
   },
   {
     name: "Geschichte Engine",
     description:
-      "lorem ipsum dolor sit amet conscution amoris tenebris donae donaotum",
+      'The Geschichte Engine (short "G-Engine") is a library written to create dynamic and yet simple visual novels.',
+    gitUrl: "https://github.com/wolkemann/g-engine",
+    demoUrl: "https://wolkemann.net/geschengine",
   },
   {
     name: "Project HH",
